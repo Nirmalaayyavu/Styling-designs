406f85289e2e5c7dba9c386d6a86262f8ca9f836
Nirmala Ayyavu <nirmalaayyavu@gmail.com>
2021-04-09 12:39:26 +0530

    styling changes and icon addition

--- src/App.js
+++ src/App.js
@@ -15,16 +15,16 @@ function App() {
           </Col>
           <Col className="right-section">
             <Row >
-              <Col>
+              <Col className="image-section">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
                   <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
                   <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z" />
                 </svg></Col>
-              <Col>
+              <Col className="image-section">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save" viewBox="0 0 16 16">
                   <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z" />
                 </svg></Col>
-              <Col xs={9}>
+              <Col xs={10}>
                 <div className="button-section">
                   <Button variant="secondary" className="teritary-button">Reject</Button>{' '}
                   <Button variant="outline-secondary" className="secondary-button">Authorise</Button>{' '}
@@ -42,28 +42,40 @@ function App() {
                   <span>SET2938ABCH</span>
                 </div>
               </Col>
-              <Col className="bottom-section">
+              <Col className="bottom-section" xs={2}>
                 <div>
                   <span>Category</span>
                   <span>payment</span>
                 </div>
               </Col>
-              <Col xs={7} className="bottom-section">
+              <Col xs={6} className="bottom-section">
                 <div>
                   <span>Request status</span>
+                  <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-octagon" viewBox="0 0 16 16">
+                    <path d="M4.54.146A.5.5 0 0 1 4.893 0h6.214a.5.5 0 0 1 .353.146l4.394 4.394a.5.5 0 0 1 .146.353v6.214a.5.5 0 0 1-.146.353l-4.394 4.394a.5.5 0 0 1-.353.146H4.893a.5.5 0 0 1-.353-.146L.146 11.46A.5.5 0 0 1 0 11.107V4.893a.5.5 0 0 1 .146-.353L4.54.146zM5.1 1 1 5.1v5.8L5.1 15h5.8l4.1-4.1V5.1L10.9 1H5.1z" />
+                    <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
+                  </svg></span>
                   <span>pending transaction</span>
                 </div>
               </Col>
             </Row>
           </Col>
           <Col>
-            <div>
-              <Button variant="link" className="details-link">Full details
+            <Row>
+              <Col></Col>
+
+              <Col xs={4} className="link-section">
+                <div>
+                  <Button variant="link" className="details-link">Full details
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
-                  <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z" />
-                </svg>
-              </Button>
-            </div>
+                      <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z" />
+                    </svg>
+                  </Button>
+                </div>
+              </Col>
+              <Col></Col>
+            </Row>
+
           </Col>
         </Row>
       </Container>
